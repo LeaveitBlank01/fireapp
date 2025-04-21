ffb79ac5844917b44c0f9abd5efae96da49fa89f
leaveitBlank01 <202280102@psu.palawan.edu.ph>
2025-04-22 04:16:08 +0800

    dynamic multibar chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarBySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('stations', views.map_station, name='map-station'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
     path('lineChart/', LineCountbyMonth, name='chart'),
     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
-    path('stations', views.map_station, name='map-station'),
+    path('multiBarchart/', multipleBarBySeverity, name='chart'),
 
 ]
